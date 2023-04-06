30e0ce368ff74bb5dd62fdc62346f8319dd7f7e2
Daniel Reimer <daniel.reimer@reactos.org>
2023-04-06 02:26:31 +0200

    Update styles.js
    2023.04 semi working state. Sidebar still ist not behaving right.

--- www/styles.js
+++ www/styles.js
@@ -1,7 +1,9 @@
-const style = document.createElement('style');
-style.innerHTML = `
-  #contentContainer {
-    background-color: transparent !important;
-  }
-`;
-document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("#drawer").shadowRoot.appendChild(style);
+// Semi working Sidebar fix by Bram
+document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer").shadowRoot.querySelector(".mdc-drawer").style.setProperty("--mdc-theme-surface", "transparent");
+//document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer").shadowRoot.querySelector(".mdc-drawer").style.setProperty("background", "rgba(0,0,0,0.5)");
+
+//Transparent background of Main-Section
+document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view").style.setProperty("background", "transparent");
+
+//Semi-Transparent background of Header
+document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("div:nth-child(1) > div.header").style.setProperty("background-color", "rgba(0,0,0,0.5)");
