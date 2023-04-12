27b82ffd1c24a3bdff3cffad3ad161b1536e6dea
Daniel Reimer <daniel.reimer@reactos.org>
2023-04-12 12:58:22 +0200

    Update styles.js
    Bypass "Lovelace Animated Background"-Addin completely and move the video on the very top to be a REAL background behind anything and everything.
    Status: WORKS!!

--- www/styles.js
+++ www/styles.js
@@ -1,9 +1,16 @@
-// Semi working Sidebar fix by Bram
-document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer").shadowRoot.querySelector(".mdc-drawer").style.setProperty("--mdc-theme-surface", "transparent");
-//document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer").shadowRoot.querySelector(".mdc-drawer").style.setProperty("background", "rgba(0,0,0,0.5)");
-
 //Transparent background of Main-Section
-//document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view").style.setProperty("background", "transparent");
+document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view > hui-view").style.setProperty("background", "transparent");
 
-//Semi-Transparent background of Header
-//document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("div:nth-child(1) > div.header").style.setProperty("background-color", "rgba(0,0,0,0.5)");
+const max = 62;
+const min = 1;
+
+const counter = Math.floor(Math.random() * (max - min + 1) + min);
+
+const video = document.createElement('video');
+video.autoplay = true;
+video.loop = true;
+video.muted = true;
+video.id = "myVideo";
+video.innerHTML = `<source src="/local/animated_backgrounds/`+counter+`.mp4" type="video/mp4">`;
+
+document.querySelector("body").insertBefore(video, document.querySelector("body").firstChild);
